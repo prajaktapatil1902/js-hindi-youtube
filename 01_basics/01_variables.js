c28126061d4885922fd0d0821730eeb85d722c95
prajaktapatil1902 <prajaktapatil8286@gmail.com>
2026-07-05 07:48:42 +0000

    adding variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123;
+let accountEmail= "praj@gmail.com";
+var accountPassword = "123456789";
+accountCity= "Bangalore";
+
+ //accountId=456;  //not allowed because accountId is a constant variable
+accountEmail= "abs@gmail.com"; 
+accountPassword= "987654321";
+accountCity= "Mumbai";
+
+
+console.log(accountId);
+console.table({accountId, accountEmail, accountPassword, accountCity});
+
